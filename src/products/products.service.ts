f8df8348c5a1d9416bef17aa564553e747a6909d
Romentos <padilla.naranja@gmail.com>
2023-09-03 10:43:59 +0200

    add find all, find by id and remove

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class ProductsService {
@@ -25,21 +26,27 @@ export class ProductsService {
       
     }
   }
-
+  // TODO:paginar
   findAll() {
-    return `This action returns all products`;
+    return this.productRepository.find({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({id});
+    if(!product){
+      throw new NotFoundException(`Product with id ${id} not found`);
+    } 
+    return product;
+    
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
   }
 
   private handleExceptions(error:any){
