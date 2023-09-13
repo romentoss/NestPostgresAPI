08edc9518fc18b93e8603abeecffb1632d68c1ca
Romentos <padilla.naranja@gmail.com>
2023-09-13 14:06:23 +0200

    add relations on products and users table

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,8 +3,9 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
-import { Auth } from './../auth/decorators';
+import { Auth, GetUser } from './../auth/decorators';
 import { ValidRoles } from 'src/auth/interfaces/valid-roles';
+import { User } from './../auth/entities/user.entity';
 
 @Controller('products')
 export class ProductsController {
@@ -12,8 +13,11 @@ export class ProductsController {
 
   @Post()
   @Auth( ValidRoles.admin )
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  create(
+    @Body() createProductDto: CreateProductDto,
+    @GetUser() user:User,
+    ) {
+    return this.productsService.create(createProductDto, user);
   }
 
   @Get()
@@ -31,8 +35,9 @@ export class ProductsController {
   @Patch(':id')
   @Auth( ValidRoles.admin )
   update(@Param('id', ParseUUIDPipe) id: string, 
-         @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(id, updateProductDto);
+         @Body() updateProductDto: UpdateProductDto,
+         @GetUser() user:User) {
+    return this.productsService.update(id, updateProductDto, user);
   }
 
   @Delete(':id')
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,6 +6,7 @@ import { DataSource, Repository } from 'typeorm';
 import { PaginationDto } from '../common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid';
 import { ProductImage,Product } from './entities';
+import { User } from '../auth/entities/user.entity';
 
 @Injectable()
 export class ProductsService {
@@ -20,13 +21,14 @@ export class ProductsService {
     private readonly dataSource:DataSource
   ){}
 
-  async create(createProductDto: CreateProductDto) {
+  async create(createProductDto: CreateProductDto, user:User) {
     try {
       const {images=[], ...productDetails } = createProductDto;
 
       const product = this.productRepository.create({
         ...productDetails,
-        images: images.map( image => this.productImageRepository.create({ url: image }) )
+        images: images.map( image => this.productImageRepository.create({ url: image }) ),
+        user
       });
       await this.productRepository.save( product );
       return { ...product, images };
@@ -88,7 +90,7 @@ export class ProductsService {
     }
   }
 
-  async update(id: string, updateProductDto: UpdateProductDto) {
+  async update(id: string, updateProductDto: UpdateProductDto, user:User) {
 
     const {images, ...toUpdate } = updateProductDto;
 
@@ -116,6 +118,7 @@ export class ProductsService {
           (image) => this.productImageRepository.create({url:image})
         )
       }
+      product.user = user;
       await queryRunner.manager.save(product);
       
       // await this.productRepository.save(product);
